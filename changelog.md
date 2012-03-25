e709b32cfd7e6e9561d4ae03adb244440c854de6
Yosem Sweet <yosem.sweet@suite101.com>
2012-03-25 15:36:20 -0700

    iron_cuke version 0.0.5 commit

---
+++ changelog.md
@@ -0,0 +1,7 @@
+Todo
+====
+
+0.0.2 - First working prototype support scheduling tasks
+0.0.3 - Added queue support
+0.0.4 - Bug fixes
+0.0.5 - Fixed an integration issue that was breaking active controller. Added a dummy app skeleton for future testing purposes.
--- spec/integration/visit_page_spec.rb
+++ spec/integration/visit_page_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
 
-describe "Visit pages", :type => :request do
+describe "Visit pages" do
   
 	describe "visit homepage" do
-		debugger
-		get '/'
+		it "should not generate an error" do
+			expect {
+				visit "/"
+			}.to_not raise_error
+		end
 	end
 
 end
